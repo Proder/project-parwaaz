9e495e70d21f8f043f9e7dd3fce2356c8610769e
Aayush2104 <aayush.2104@gmail.com>
2023-08-02 22:35:11 +0530

    Added faqs page with accordion(experimental)

--- project-parwaaz/src/App.jsx
+++ project-parwaaz/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import About from "./pages/About"
 import Volunteer from "./pages/Volunteer";
 import ContactUs from "./pages/ContactUs";
+import Faq from "./pages/Faq";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Volunteer" element={<Volunteer />} />
+        <Route path="/faqs" element = {<Faq/>}/>
         <Route path="/Contact" element={<ContactUs/>} />
       </Routes>
     </Layout>
